e96c07a08232eb8ad1733d6bb1e3808e521a7964
Manish9737 <manishkumavat73@gmail.com>
2026-03-18 21:35:41 +0530

    app.js old

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require("http");
+const http = require('http');
 
 var createError = require("http-errors");
 var express = require("express");
@@ -7,7 +7,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 const session = require("express-session");
-const compression = require("compression");
 
 var indexRouter = require("./routes/index/index");
 var usersRouter = require("./routes/users/users");
@@ -17,38 +16,39 @@ var gsRouter = require("./routes/gameStation/gameStation");
 var adminRouter = require("./routes/admin/admin");
 var bookingsRouter = require("./routes/bookings/bookings");
 var gamesRouter = require("./routes/games/games");
-var feedbackRouter = require("./routes/feedback/feedback");
+var feedbackRouter = require("./routes/feedback/feedback")
 var blogRouter = require("./routes/blog/blog");
 var slotRouter = require("./routes/slots/slots");
 var paymentRouter = require("./routes/payment/payment");
 var bankDetailsRouter = require("./routes/bankDetails/bankDetails");
 var DashboardRouter = require("./routes/Dashboard/Dashboard");
 
-require("./DB/conn");
-require("./middlewares/passportConfig");
+const Razorpay = require('razorpay');
 
-const securityMiddleware = require("./middlewares/security");
-const rateLimiter = require("./middlewares/rateLimiter");
+require("./DB/conn");
+
+require("./middlewares/passportConfig");
 
 var app = express();
 
 app.use(
   session({
     secret: ["cyberwolve"],
-    resave: false,
     saveUninitialized: false,
-  }),
+  })
 );
 
+
 const corsOptions = {
-  origin: [
+  origin: [ 
     "https://playways-app.web.app",
     "http://localhost:5173",
     "http://localhost:3000",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    "*",
+    ], 
+  methods: "GET,POST,PUT,DELETE", 
   credentials: true,
-  optionsSuccessStatus: 204,
+  optionsSuccessStatus: 204, 
 };
 
 // view engine setup
@@ -56,16 +56,11 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
-app.use(express.json({ limit: "1mb" }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
-app.set("trust proxy", 1);
-securityMiddleware(app);
-app.use(rateLimiter);
-app.use(compression());
 
 app.use("/images", express.static("public/images"));
 app.use("/", indexRouter);
@@ -87,15 +82,12 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use((err, req, res, next) => {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  res.status(err.status || 500).json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
-  });
-  // res.render("error");
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
